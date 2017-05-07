cf29d23d16de9f1d60e950da0071c18681145c09
Ilya Nee <neeilya@mail.ru>
2017-05-08 00:38:43 +0200

    Component for feed image

---
+++ src/components/FeedImage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="img card" v-bind:style="`background-image:url('${ data.url_n }')`"></div>
+</template>
+
+<script type="text/babel">
+  export default {
+    props: ['data'],
+  };
+</script>
